6a70aa8bd8087bb5cef79284923b01e7e97c1fd0
richardPractite <windell.sacks@gmail.com>
2024-01-30 19:53:20 -0600

    Commit Message

--- feedback_page.py
+++ feedback_page.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 def feedback_form():
     st.title("Website Feedback Form")
 
@@ -7,14 +8,18 @@ def feedback_form():
 
     primary_reason = st.text_input("What is the PRIMARY reason you came to the site?")
 
-    found_what_needed = st.radio("Did you find what you needed?", ["Yes, all of it", "Yes, some of it", "No, none of it"])
+    found_what_needed = st.radio("Did you find what you needed?",
+                                 ["Yes, all of it", "Yes, some of it", "No, none of it"])
 
     if found_what_needed == "No, none of it":
-        additional_info = st.text_area("If you did not find any or all of what you needed, please tell us what information you were looking for.")
+        additional_info = st.text_area(
+            "If you did not find any or all of what you needed, please tell us what information you were looking for.")
 
-    ease_of_finding_info = st.radio("Please tell us how easy it is to find information on the site.", ["Very Easy", "Easy", "Average", "Difficult", "Very Difficult"])
+    ease_of_finding_info = st.radio("Please tell us how easy it is to find information on the site.",
+                                    ["Very Easy", "Easy", "Average", "Difficult", "Very Difficult"])
 
-    overall_impression = st.radio("What is your overall impression of the site?", ["Below Expectations", "Meets Expectations", "Exceeds Expectations"])
+    overall_impression = st.radio("What is your overall impression of the site?",
+                                  ["Below Expectations", "Meets Expectations", "Exceeds Expectations"])
 
     professional_rating = st.slider("Professional", 0, 10, step=1)
 
@@ -22,7 +27,9 @@ def feedback_form():
 
     visually_pleasing_rating = st.slider("Visually Pleasing", 0, 10, step=1)
 
-    likelihood_to_visit_again = st.selectbox("What is the likelihood that you will visit the website again?", ["Extremely likely", "Very likely", "Moderately likely", "Slightly likely", "Not at all likely"])
+    likelihood_to_visit_again = st.selectbox("What is the likelihood that you will visit the website again?",
+                                             ["Extremely likely", "Very likely", "Moderately likely", "Slightly likely",
+                                              "Not at all likely"])
 
     additional_comments = st.text_area("Please add any comments you have for improving the website.")
 
@@ -30,10 +37,11 @@ def feedback_form():
         # You can add code here to handle the form data, e.g., save it to a database or perform further analysis.
         st.success("Feedback submitted successfully!")
 
+
 if __name__ == "__main__":
     website_feedback_form()
 
-<<<<<<< HEAD
+
 def feedback_page():
     st.title("Aquatic Rig Feedback Page")
     st.textbox()
@@ -49,5 +57,3 @@ def feedback_page():
 ##          <button>Submit</button>
 ##      </body>
 ##  </html>
-=======
->>>>>>> origin/feature/feedback-page
