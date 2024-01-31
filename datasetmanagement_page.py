dd96cfdc9c37dc55f0b952eaa50962433f46845f
Isaiahensley <isaiahensley@gmail.com>
2024-01-31 00:20:16 -0600

    1) Checks for "if file is uploaded then..." 2) Reads .nc files that are uploaded 3) Displays surface level information about the file

--- datasetmanagement_page.py
+++ datasetmanagement_page.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from datetime import datetime
+import netCDF4 as nc
 
 def datasetmanagement_page():
     # Page title
@@ -17,6 +18,25 @@ def datasetmanagement_page():
     # Sliders for Time and Depth
     st.slider("Time", value=datetime(2020, 1, 1, 9, 30), format="MM/DD/YY - hh:mm")
     st.slider("Depth", 0, 100)
+
+    if file_upload is not None:
+        st.text("file uploaded")
+        file_content = file_upload.read()  # Read file content as bytes
+        nc_file = nc.Dataset('in-memory', memory=file_content, diskless=True)
+
+        # Get and display information about the dataset
+        st.text(f"Number of variables: {len(nc_file.variables)}")
+        st.text(f"Variables: {list(nc_file.variables.keys())}")
+
+
+
+
+
+
+
+
+
+
 
     # Depending on the option selected, the corresponding graph should be used on the dataset and displayed
     # on the website
