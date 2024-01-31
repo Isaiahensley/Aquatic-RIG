64bed77af569070b867d9de95a4f0937d623456f
richardPractite <windell.sacks@gmail.com>
2024-01-30 19:47:23 -0600

    Commit Message

--- feedback_page.py
+++ feedback_page.py
@@ -2,4 +2,17 @@ import streamlit as st
 
 
 def feedback_page():
-    st.title("This is the feedback page")
+    st.title("Aquatic Rig Feedback Page")
+    st.textbox()
+    st.button("Submit")
+
+## <html>
+## <head>
+## <title>Aquatic Rig Feedback Page</title>
+## </head>
+## <body>
+## <h1>Give us feedback</h1>
+## <textbox></textbox>
+## <button>Submit</button>
+## </body>
+## </html>
