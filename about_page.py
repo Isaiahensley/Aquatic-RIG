092c06b189ae5330e207a522e92334baa581f39d
richardPractite <windell.sacks@gmail.com>
2024-02-27 21:04:38 -0600

    Commit Message

--- about_page.py
+++ about_page.py
@@ -3,3 +3,7 @@ import streamlit as st
 
 def about_page():
     st.title("This is the about page")
+    with st.container():
+        st.markdown("Home Page")
+        st.markdown("Dataset Management Page")
+        st.markdown("Feedback Page")
