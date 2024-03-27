8bbcb32962ac738883d05fd5bf740411b7a51c21
Sudeep Paudel <53842239+spaude11@users.noreply.github.com>
2024-03-27 09:52:43 -0500

    Update feedback_page.py

--- feedback_page.py
+++ feedback_page.py
@@ -3,13 +3,9 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import base64
 from io import BytesIO
-import dropbox
-from io import BytesIO
-import logging
-from dropbox_utils import DropboxLogger
-from token_file import DROPBOX_ACCESS_TOKEN
-from datetime import datetime
 
+
+# Generating PDF Function
 def generate_pdf(data):
     pdf_filename = "feedback_report.pdf"
     pdf_canvas = canvas.Canvas(pdf_filename, pagesize=letter)
@@ -24,15 +20,12 @@ def generate_pdf(data):
         y_position -= 15
 
     pdf_canvas.save()
-
-    # Read the content of the PDF file
-    with open(pdf_filename, "rb") as f:
-        pdf_content = f.read()
-
-    return pdf_filename, pdf_content
+    return pdf_filename
 
 
+# Feedback Page Structure
 def feedback_page():
+    # Title
     st.title("Website Feedback Form")
 
     # Overall Impression Container
@@ -47,18 +40,18 @@ def feedback_page():
     # NetCDF4 Container
     with st.container():
         st.markdown("<h3 style='color: #0077cc;'>NetCDF4</h3>", unsafe_allow_html=True)
-        have_used_net_files = st.radio("Have you ever used NetCDF4 files before?", ["Yes", "No"])
+        have_used_net_files = st.radio("Have you ever used NetCDF4 files before?", ["Yes", "No"], index=None)
         upload_own_files = st.radio("Did you upload your own files or use the example dataset?",
-                                    ["I uploaded my own files.", "I used the example dataset."])
+                                    ["I uploaded my own files.", "I used the example dataset."], index=None)
 
     # Data Visualization Container
     with st.container():
         st.markdown("<h3 style='color: #0077cc;'>Data Visualization</h3>", unsafe_allow_html=True)
-        useful_visual = st.radio("Do you think the visualizations generated are useful?", ["Yes", "No"])
-        useful_features = st.text_area("Which specific features or aspects of the visualizations did you find most"
+        useful_visual = st.radio("Do you think the visualizations generated are useful?", ["Yes", "No"], index=None)
+        useful_features = st.text_area("Which specific features or aspects of the visualizations did you find most "
                                        "useful or interesting?")
-        confusing_features = st.radio("Were there any features or aspects of the visualizations that you found"
-                                      "confusing or unnecessary?", ["Yes", "No"])
+        confusing_features = st.radio("Are there any features or aspects of the visualizations that you found "
+                                      "confusing or unnecessary?", ["Yes", "No"], index=None)
         if confusing_features == "Yes":
             what_confusing = st.text_area("What did you find confusing or unnecessary?")
         suggestions_visual = st.text_area("Do you have any suggestions for improving the visualizations generated in"
@@ -90,9 +83,10 @@ def feedback_page():
     # Privacy and Data Usage Container
     with st.container():
         st.markdown("<h3 style='color: #0077cc;'>Privacy and Data Usage</h3>", unsafe_allow_html=True)
-        st.markdown("We will use your feedback data and ensure that your responses will be kept confidential and used"
+        st.markdown("We will use your feedback data and ensure that your responses will be kept confidential and used "
                     "only for improving this website.")
 
+    # Sending Feedback Function
     if st.button("SEND"):
         # Generate PDF
         feedback_data = {
@@ -111,28 +105,12 @@ def feedback_page():
             "Field of Study": field_of_study,
             "Additional Comments": additional_comments
         }
-        user_email = email_address;
-        pdf_filename, pdf_content = generate_pdf(feedback_data)
+        pdf_filename = generate_pdf(feedback_data)
 
-        dropbox_logger = DropboxLogger(DROPBOX_ACCESS_TOKEN)
-        dropbox_logger.upload_file(pdf_content, pdf_filename, st, user_email)
-
-        # Display PDF
-        with open(pdf_filename, "rb") as f:
-            pdf_contents = f.read()
-
-        # Convert PDF to base64 encoding
-        pdf_base64 = base64.b64encode(pdf_contents).decode("utf-8")
-
-        # Display download link
-        st.markdown(f"### Generated PDF:")
-        st.markdown(
-            f'<a href="data:application/pdf;base64,{pdf_base64}" download="{pdf_filename}">Download PDF</a>',
-            unsafe_allow_html=True,
-        )
-        st.markdown("---")
-        st.write(pdf_filename)
+        # Display success message
+        st.markdown("### Generated PDF:")
         st.success("Thank you for your feedback! 🚀")
 
+    # Main Function
     if __name__ == "__main__":
         feedback_page()
